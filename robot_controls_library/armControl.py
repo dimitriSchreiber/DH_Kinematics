93396fd1decec57b8a3b35abcfb2ef14dfb3c66f
Dimitri <dschreib@eng.ucsd.edu>
2019-06-24 20:41:36 -0700

    move in robot_controls_library, lots of git issues

---
+++ robot_controls_library/armControl.py
@@ -0,0 +1,138 @@
+#Motor arm mixing, accounts for coupling in motion of each degree of freedom
+#Wants radians and mm?
+import numpy as np
+import transforms3d as t3d
+#from forwardKinematics import *
+
+pi = np.pi
+
+
+class remoteRobotArm():
+    #This class defines a robot arm controller, providing robot end effector position control
+    #Inputs:
+    #    Current robot position/orientation in cartesian/quaternion
+    #    Robot end-effector setpoint position/orientation in cartesian/quaternion
+
+    #Outputs:
+    #    Motor angles
+
+
+    def __init__(self):
+        self.jointAngleCurrent = np.zeros(7) #read from joint encoders, N/A for now
+        self.jointAngleSetpoint = np.zeros(7) #N/A
+        self.jointAngleError = np.zeros(7) #N/A
+        self.motorAngleSetpoint = np.zeros(7) #Motor angle commands given mixing matrix
+
+        self.robotJacobian = np.zeros((7,6)) #from forwardKinematics.py
+        
+        self.endEffectorCurrent = np.zeros((2,3)) #fromOptitrack
+        self.endEffectorSetpoint = np.zeros((2,3)) #input
+        self.endEffectorError = np.zeros((2,3))
+        self.jointUpperLimits = np.array([82,42,1.75,0.872,0.872,0.872,47.5])
+        self.jointLowerLimits = np.array([-82,-42,-1.75,-0.872,-0.872,-0.872,0])
+        
+        self.initMotorArmMixing()
+
+    def initMotorArmMixing(self): 
+        #armTheta is a 7x1 for desired joint angles
+        #linear motions are in ?meters?, DOUBLE CHECK THESE!!!d
+        #rotary motions are in radians
+        self.motorTheta_armTheta_full = np.zeros((7,7))
+
+
+        #linear motions are very questionable right now and NEED TO BE REVIEWED!
+
+        #backend mixing matrix;
+        X_Y_pulley = 18 * 2  / (2 * pi)#linear motion (mm) per radian
+        rotaryAxis_pulley = 24 / 100 #ratio for rotary axis
+
+        self.motorTheta_armTheta_full[0,0] = X_Y_pulley
+        self.motorTheta_armTheta_full[1,1] = X_Y_pulley
+        self.motorTheta_armTheta_full[2,2] = rotaryAxis_pulley
+
+
+        #cable driven arm mixing matrix:
+        #Pulley diameters (mm)
+        Db = 12.7 #drive pulley diameter
+        Ds = 10 #small pulley
+        Dm = 16    #medium pulley
+        Dl = 22    #large pulley
+        Dj1 = 28 #joint 1 terminating pulley
+        Dj2 = 28    #joint 2 terminating pulley
+        Dj3 = 16    #joint 3 terminating pulley
+
+        #motorTheta_armTheta = np.eye(4) * np.array([Db/Dj1, Db/Dj2, Db/Dj3, Db])
+        #motorTheta_armTheta = np.dot(np.array([[1, 0, 0, 0],
+        #                                       [Dl/Dj2, 1, 0, 0],
+        #                                       [0, 0, 1, 0], 
+        #                                       [0, 0, 0, 1]]), motorTheta_armTheta)
+        #motorTheta_armTheta = np.dot(np.array([[1, 0, 0, 0], 
+        #                                       [0, 1, 0, 0], 
+        #                                       [Dm/Dj3, -Dm/Dj3, 1, 0], 
+        #                                       [0, 0, 0, 1]]), motorTheta_armTheta)
+        #motorTheta_armTheta = np.dot(np.array([[1, 0, 0, 0], 
+        #                                       [0, 1, 0, 0], 
+        #                                       [0, 0, 1, 0], 
+        #                                       [-Ds, Ds, Dl, 1]]), motorTheta_armTheta) #divided last row by 2!!!!, if issues try removing this first
+        D = np.array([[Dj1,0,0,0],
+                      [-Dl,Dj2,0,0],
+                      [Dm,-Dl,Dj3,0],
+                      [Ds,-Dm,-Dl,1]]) #this needs to be finished!!
+        a1 = np.eye(4)*np.array([Db/D[0,0], 1, 1, 1])
+        a2 = np.array([[1,0,0,0],
+                     [D[1,0]/D[1,1], Db/D[1,1], 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+        a3 = np.array([[1,0,0,0],
+                     [0, 1, 0, 0],
+                     [D[2,0]/D[2,2], D[2,1]/D[2,2], Db/D[2,2], 0],
+                     [0, 0, 0, 1]])
+        a4 = np.array([[1,0,0,0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0],
+                     [D[3,0]/(2), D[3,1]/(2), D[3,2]/(2), Db/(2)]])
+        #a4 = np.eye(4)
+        #a4[3,3] = Db/2
+        
+        motorTheta_armTheta = a4 @ (a3 @ (a2 @ a1))
+        
+        self.motorTheta_armTheta_full[3:, 3:] = motorTheta_armTheta
+        self.armTheta_motorTheta = np.linalg.inv(self.motorTheta_armTheta_full) #is a 4x4 submatrix for 4dof arm
+
+    def updateMotorArmMixing(self):
+        temp = np.clip(self.jointAngleSetpoint, self.jointLowerLimits, self.jointUpperLimits)
+        if (temp != self.jointAngleSetpoint).any():
+            bad=True
+            #print("warning! commanded joint angle is out of robot limits")
+        self.jointAngleSetpoint = temp   
+        self.motorAngleSetpoint = self.armTheta_motorTheta @ self.jointAngleSetpoint #takes in arm thetas and gives appropriate motor thetas
+    def commandJoints(self, motors, setpoint_arm, trajectory = True):
+        setpoint_arm[0] = 1 * setpoint_arm[0] * 1000
+        setpoint_arm[1] = -1 * setpoint_arm[1] * 1000
+        setpoint_arm[2] = 1 * setpoint_arm[2] 
+        setpoint_arm[3] = -1 * setpoint_arm[3]
+        setpoint_arm[4] = 1 * setpoint_arm[4]
+        setpoint_arm[5] = -1 * setpoint_arm[5]
+        setpoint_arm[6] = 1 * setpoint_arm[6] * 1000
+
+        self.jointAngleSetpoint = setpoint_arm
+        self.updateMotorArmMixing()
+        
+        axis_motor_indexes = np.array([5, 4, 6, 0, 3, 2, 1]) #6 might be wrong 
+        velocity = np.ones(8)*np.pi/3
+        
+        setpoint_motor = np.zeros(8)
+        setpoint_motor[axis_motor_indexes[0:7]] = self.motorAngleSetpoint[0:7]
+
+        if trajectory:
+            motors.run_trajectory(setpoint_motor, velocity)
+        else:
+            motors.command_motors_radians(setpoint_motor)
+
+
+    def updateRobotEECurrent(self):
+        pass
+    def updateRobotJacobian(self):
+        pass
+    def zeroRobot(self):
+        pass
---
+++ robot_controls_library/notebooks/optitrakTest.py
@@ -0,0 +1,57 @@
+import signal
+import sys
+import transforms3d as tf3d
+import time
+import numpy as np
+
+sys.path.append("..")
+
+from utils.NatNetClient2 import NatNetClient
+from utils.GetJointData import data, NatNetFuncs #receiveNewFrame, receiveRigidBodyFrameList
+from utils.getRobotPose import getOptitrackPose
+
+print_trak_data = False
+print_cartesian = True
+
+server_ip = "192.168.0.104"
+multicastAddress = "239.255.42.99"
+
+joint_names = ['Base','Needle']
+ids = [0, 1]
+
+#Tracking class
+print("Starting streaming client now...")
+streamingClient = NatNetClient(server_ip, multicastAddress, verbose = print_trak_data)
+NatNet = NatNetFuncs()
+streamingClient.newFrameListener = NatNet.receiveNewFrame
+streamingClient.rigidBodyListListener = NatNet.receiveRigidBodyFrameList
+
+prev_frame = 0
+time.sleep(0.5)
+
+streamingClient.run()
+
+time.sleep(0.5)
+
+track_data = data(joint_names,ids)
+
+time.sleep(0.5)
+
+track_data.parse_data(NatNet.joint_data, NatNet.frame) #updates the frame and data that is being used
+#debug values
+
+
+
+while True:
+    time.sleep(0.1)
+    base = track_data.bodies[0].homogenous_mat
+    base_inv = track_data.bodies[0].homg_inv
+    rgdbdy1 = track_data.bodies[1].homogenous_mat
+    
+    rgdbdy1_base, rgdbdy1_pos, rgdbdy1_euler, _ = track_data.homg_mat_mult(base_inv,rgdbdy1)
+    
+    rgdbdy1_deg = np.array(rgdbdy1_euler)*180/np.pi
+    rgdbdy1_pos_mm = np.array(rgdbdy1_pos)*1000
+    
+    
+    print("rgdbdy: {}, rgdbdy_base: {}, rgdbdy_euler: {}, rgdbdy_deg: {}, rgdbdy_pos:, {}" .format(rgdbdy1, rgdbdy1_base, rgdbdy1_euler, rgdbdy1_deg, rgdbdy1_pos_mm))
